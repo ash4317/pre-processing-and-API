d5490abf2d59ac3cb54a3f439c7698adedb9ac22
Ashwin Milind Kulkarni <ashwinkulkarni82@gmail.com>
2020-06-29 09:46:16 +0530

    Removed clustering algos from main.py. Cluserting algos are put ni a separate module called 'clustering'. Ran a test case for 50 docs and output is stored in 'preprocessed-1.json'.

--- main.py
+++ main.py
@@ -7,20 +7,14 @@ This code reads and extract all the data from the .htm URLs present in the excel
 # User-Developed Modules
 import clean_file as cf
 import preprocessing as prep
+import cluster
 
 # Inbuilt modules for extracting data
 import sys
 import shutil
 import json
 import os
-from unidecode import unidecode
-
-# Modules for Clustering
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
 
 
 def get_links():
@@ -45,53 +39,6 @@ def get_links():
         sys.exit("No. of docs exceeds limit.")
     return links[:no_of_docs]
 
-
-def calc_TFIDF(word_list, threshold):
-
-    # object to transform words into vectors
-    vectorizer = CountVectorizer()
-
-    # for applying variance thrshold to reduce features
-    selector = VarianceThreshold(threshold=threshold)
-
-    '''
-    Transform into TFIDF matrix, apply variance threshold to reduce features,
-    print the order of TFIDF matrix and return the TFIDF matrix
-    '''
-    X = vectorizer.fit_transform(word_list)
-    X = selector.fit_transform(X)
-    transformer = TfidfTransformer(smooth_idf=False)
-    tfidf = transformer.fit_transform(X)
-    print("\n")
-    print(f"TFIDF matrix order: {tfidf.shape}")
-    return tfidf
-
-
-def eval_clusters(tfidf, word_list, epsilon, minSamples):
-    '''
-    This function applies DBSCAN clustering algorithm and prints clusters, number of features and Silhouette coefficient
-    '''
-    db = DBSCAN(eps=epsilon, min_samples=minSamples, metric='cosine').fit(tfidf)
-
-    # get all clusters and clusters labels
-    clusters = db.labels_.tolist()
-    labels = db.labels_
-
-    #Creating dictionary having doc with the corresponding cluster number.
-    idea_3={'Idea':word_list, 'Cluster':clusters}
-
-    # Converting it into a dataframe.
-    frame_3=pd.DataFrame(idea_3,index=[clusters], columns=['Idea','Cluster'])
-
-    print("\n")
-    print(f"For epsilon:{epsilon}, minSamples:{minSamples}")
-
-    #Print the counts of doc belonging to each cluster.
-    print(frame_3['Cluster'].value_counts())
-    print("\n")
-
-    # Print the Silhouette Coefficient
-    print(f"Silhouette Coefficient: {metrics.silhouette_score(tfidf, labels)}")
 
 
 def removeCache():
@@ -135,14 +82,14 @@ if __name__ == "__main__":
     '''
     Calculate TFIDF matrix for the preprocessed text
     '''
-    tfidf = calc_TFIDF(word_list, 0.05)
+    tfidf = cluster.calc_TFIDF(word_list, 0.05)
 
     # evaluate clusters with given epsilon and min_sample values
-    eval_clusters(tfidf, word_list, epsilon=0.18, minSamples=3)
-    eval_clusters(tfidf, word_list, epsilon=0.3, minSamples=5)
-    eval_clusters(tfidf, word_list, epsilon=0.2, minSamples=10)
-    eval_clusters(tfidf, word_list, epsilon=0.1, minSamples=5)
-    eval_clusters(tfidf, word_list, epsilon=0.35, minSamples=10)
+    cluster.eval_clusters(tfidf, word_list, epsilon=0.18, minSamples=3)
+    cluster.eval_clusters(tfidf, word_list, epsilon=0.3, minSamples=5)
+    cluster.eval_clusters(tfidf, word_list, epsilon=0.2, minSamples=10)
+    cluster.eval_clusters(tfidf, word_list, epsilon=0.1, minSamples=5)
+    cluster.eval_clusters(tfidf, word_list, epsilon=0.35, minSamples=10)
 
     # to remove useless cache data
     removeCache()
