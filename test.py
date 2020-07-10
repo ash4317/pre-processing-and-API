607594b1dd8544062fd2047a8ac2d7c03d5023a0
Ashwin Milind Kulkarni <ashwinkulkarni82@gmail.com>
2020-07-10 10:20:33 +0530

    Changed test URLs because of change in API

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def readlinks(path):
 
 filename = 'ISINS_v3.xlsx'
 ISINs, URLs = readlinks(filename)
-datajson = {}
+datajson = dict()
 i = 0
 for ISIN in ISINs:
     datajson[ISIN] = URLs[i]    
@@ -29,85 +29,85 @@ for ISIN in ISINs:
 json_dumps = json.dumps(datajson)
 
 #Extract data
-url = "http://127.0.0.1:5000/extract?filepath=ISINS_v3.xlsx&no_of_docs=15&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/extract?no_of_docs=15&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url, json=datajson)
 print(r.text)
 
 
 #Export extracted data
-url = "http://127.0.0.1:5000/extract/export?filepath=extract.xlsx&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/extract/export?filepath=extract.xlsx&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 print(r.text)
 
 
 #Pre-process data
-url = "http://127.0.0.1:5000/preprocess?steps=url&steps=stemming&steps=lemmatization&steps=stopwords&steps=unusual&uname=vaishnavi&fname=ISINS_v3.xlsx" 
+url = "http://127.0.0.1:5000/preprocess?steps=url&steps=stemming&steps=lemmatization&steps=stopwords&steps=unusual&uname=ash4317&fname=ISINS_v3.xlsx" 
 r = requests.post(url=url)
 print(r.text)
 
 #Export pre-processed data
-url = "http://127.0.0.1:5000/preprocess/export?filepath=prep.xlsx&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/preprocess/export?filepath=prep.xlsx&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 print(r.text)
 
 
 #Perform K-Means clustering 
-url = "http://127.0.0.1:5000/clustering/kmeans?k=4&format=csv&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/kmeans?k=4&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "http://127.0.0.1:5000/clustering/kmeans?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/kmeans?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 
 # Get clustered data
-url = "http://127.0.0.1:5000/clustering/summary?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 
 #Perform DBSCAN clustering
-url = "http://127.0.0.1:5000/clustering/dbscan?eps=0.3&min=1&format=csv&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/dbscan?eps=0.3&min=1&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "http://127.0.0.1:5000/clustering/dbscan?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/dbscan?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
-url = "http://127.0.0.1:5000/clustering/summary?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 
 #Perform Agglomerative clustering
-url = "http://127.0.0.1:5000/clustering/agglomerative?k=5&format=csv&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/agglomerative?k=5&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "http://127.0.0.1:5000/clustering/agglomerative?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/agglomerative?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 
-url = "http://127.0.0.1:5000/clustering/summary?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 #Perform Birch clustering
-url = "http://127.0.0.1:5000/clustering/birch?k=5&format=csv&uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/birch?k=5&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "http://127.0.0.1:5000/clustering/birch?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/birch?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
-url = "http://127.0.0.1:5000/clustering/summary?uname=vaishnavi&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
