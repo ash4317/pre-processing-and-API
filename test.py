230c8e67dd472d7bf4ff068c090cf2b7af1b88e8
Ashwin Milind Kulkarni <ashwinkulkarni82@gmail.com>
2020-07-11 18:44:35 +0530

    Changed test cases

--- test.py
+++ test.py
@@ -32,84 +32,84 @@ for ISIN in ISINs:
 
 
 #Extract data
-url = "https://preprocess-and-cluster-api.herokuapp.com/extract?no_of_docs=30&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/extract?no_of_docs=30&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url, json=datajson)
 print(r.text)
 
 #Export extracted data
-url = "https://preprocess-and-cluster-api.herokuapp.com/extract/export?filepath=extract.xlsx&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/extract/export?filepath=extract.xlsx&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 print(r.text)
 
 #Pre-process data
-url = "https://preprocess-and-cluster-api.herokuapp.com/preprocess?steps=url&steps=stemming&steps=lemmatization&steps=stopwords&steps=unusual&uname=ash4317&fname=ISINS_v3.xlsx" 
+url = "http://127.0.0.1:5000/preprocess?steps=url&steps=stemming&steps=lemmatization&steps=stopwords&steps=unusual&uname=ash4317&fname=ISINS_v3.xlsx" 
 r = requests.post(url=url)
 print(r.text)
 
 #Export pre-processed data
-url = "https://preprocess-and-cluster-api.herokuapp.com/preprocess/export?filepath=prep.xlsx&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/preprocess/export?filepath=prep.xlsx&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 print(r.text)
 
 
 
 #Perform K-Means clustering 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/kmeans?k=4&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/kmeans?k=4&format=excel&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/kmeans?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/kmeans?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 # Get clustered data
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/summary?uname=ash4317"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317"
 r = requests.get(url=url)
 print(r.text)
 
 #Perform DBSCAN clustering
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/dbscan?eps=0.3&min=1&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/dbscan?eps=0.3&min=1&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/dbscan?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/dbscan?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 
 
 #Perform Agglomerative clustering
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/agglomerative?k=5&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/agglomerative?k=5&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/agglomerative?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/agglomerative?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
 
 
 #Perform Birch clustering
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/birch?k=5&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/birch?k=5&format=csv&uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.post(url=url)
 img = Image.open(io.BytesIO(r.content))
 img.show()
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/birch?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/birch?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
 
-url = "https://preprocess-and-cluster-api.herokuapp.com/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
+url = "http://127.0.0.1:5000/clustering/summary?uname=ash4317&fname=ISINS_v3.xlsx"
 r = requests.get(url=url)
 print(r.text)
