50dce26b7c2c8d10eed97da918fdc2c939ae34e6
Ashwin Milind Kulkarni <ashwinkulkarni82@gmail.com>
2020-07-12 22:05:20 +0530

    Deployed

--- NonBruteForceReportGen.py
+++ NonBruteForceReportGen.py
@@ -61,7 +61,7 @@ def generateDataFrames(scoreDfs, allUsefulSheetAttr, allTemplateAttr, allSheetAt
         frames.append(df)
     result1 = pd.concat(frames, sort=False)
     result1.index.name = "Termsheet Name"
-    allDfs["Match & Miss-match Matrix"] = result1.to_json()
+    allDfs["Match & Miss-match Matrix"] = result1.to_json(orient='split')
 
     index = []
     attrs = []
@@ -70,7 +70,7 @@ def generateDataFrames(scoreDfs, allUsefulSheetAttr, allTemplateAttr, allSheetAt
         attrs.append(attr)
 
     result2 = pd.DataFrame(data=attrs, index=index)
-    allDfs["Attributes from Sheet"] = result2.to_json()
+    allDfs["Attributes from Sheet"] = result2.to_json(orient='split')
 
     index = []
     attrs = []
@@ -79,7 +79,7 @@ def generateDataFrames(scoreDfs, allUsefulSheetAttr, allTemplateAttr, allSheetAt
         attrs.append(attr)
 
     result3 = pd.DataFrame(data=attrs, index=index)
-    allDfs["Attributes from Template"] = result3.to_json()
+    allDfs["Attributes from Template"] = result3.to_json(orient='split')
 
     index = []
     attrs = []
@@ -88,7 +88,7 @@ def generateDataFrames(scoreDfs, allUsefulSheetAttr, allTemplateAttr, allSheetAt
         attrs.append(attr)
 
     result4 = pd.DataFrame(data=attrs, index=index)
-    allDfs["All Attributes from Sheet"] = result4.to_json()
+    allDfs["All Attributes from Sheet"] = result4.to_json(orient='split')
 
     return allDfs
 
